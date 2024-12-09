8fd0dbf4f005b47d21a4b3f1d15392518ce50732
chealy90 <christopherhealy20@gmail.com>
2024-12-09 22:36:08 +0000

    Q11 finished

--- src/main/java/org/example/Question10.java
+++ src/main/java/org/example/Question10.java
@@ -1,9 +1,20 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Scanner;
 
 public class Question10 {
+    public static void main(String[] args){
+        int[][] image = new int[8][8];
+        populateMaze(image);
+        image[5][1] = 8;
+        display(image);
+        solve(5, 1, DIRECTION.NORTH, image);
+    }
+
     public static void display(int[][] image) {
         for (int x = 0; x < image.length; x++) {
             for (int y = 0; y < image[0].length; y++) {
@@ -13,11 +24,135 @@ public class Question10 {
         }
     }
 
-    public void solve(int x, int y, DIRECTION dir) {
+    public static void solve(int x, int y, DIRECTION dir, int[][] image) {
         Deque<PathPoint> paths = new ArrayDeque<>();
+        //start point
         PathPoint currentPoint = new PathPoint(x, y, dir);
-        //push all paths from current point
-        //paths.push(new PathPoint()));
+        boolean solved = false;
 
+        //push all paths from current point
+        paths.push(new PathPoint(x, y, DIRECTION.NORTH));
+
+
+
+/*
+        if (x != 0 && image[x-1][y]!=0){paths.push(new PathPoint(x-1, y, DIRECTION.NORTH));}
+        if (x != image.length-1 && image[x+1][y]!=0){paths.push(new PathPoint(x+1, y, DIRECTION.SOUTH));}
+        if (y != 0 && image[x][y-1] != 0){paths.push(new PathPoint(x, y-1, DIRECTION.WEST));}
+        if (y!=image[0].length-1 && image[x][y+1]!=0){paths.push(new PathPoint(x, y+1, DIRECTION.EAST));}
+
+
+ */
+        while(!paths.isEmpty() && !solved){
+            PathPoint path = paths.pop();
+            int xPos = path.x;
+            int yPos = path.y;
+            System.out.println("Xpos " + xPos +  " yPos: "+yPos);
+            switch (path.direction){
+                case DIRECTION.NORTH:
+                    //check for solved;
+                    if (xPos==0){
+                        solved = true;
+                    }
+
+                    while (image[xPos][yPos] != 1){
+                        //intersections - east and west
+                        if (image[xPos][yPos-1] == 0){
+                            paths.push(new PathPoint(xPos, yPos, DIRECTION.WEST));
+                        }
+                        if (image[xPos][yPos+1]==0){
+                            paths.push(new PathPoint(xPos, yPos, DIRECTION.EAST));
+                        }
+                        xPos--;
+                    }
+                    break;
+
+                case DIRECTION.SOUTH:
+                    //check for solved;
+                    if (xPos==image.length-1){
+                        solved = true;
+                        break;
+                    }
+                    while (image[xPos][yPos] != 1){
+                        //intersections - east and west
+                        if (image[xPos][yPos-1] == 0){
+                            paths.push(new PathPoint(xPos, yPos, DIRECTION.WEST));
+                        }
+                        if (image[xPos][yPos+1]==0){
+                            paths.push(new PathPoint(xPos, yPos, DIRECTION.EAST));
+                        }
+                        xPos++;
+                    }
+                    break;
+
+
+                case DIRECTION.WEST:
+                    //check for solved;
+                    if (yPos==0){
+                        solved = true;
+                    }
+                    while (image[xPos][yPos] != 1){
+                        //intersections - north and south
+                        if (image[xPos - 1][yPos] == 0){
+                            paths.push(new PathPoint(xPos, yPos, DIRECTION.NORTH));
+                        }
+                        if (image[xPos + 1][yPos]==0){
+                            paths.push(new PathPoint(xPos, yPos, DIRECTION.SOUTH));
+                        }
+
+                        yPos--;
+                    }
+                    break;
+
+                case DIRECTION.EAST:
+                    //check for solved;
+                    if (yPos==0){
+                        solved = true;
+                        break;
+                    }
+                    while (image[xPos][yPos] != 1){
+                        //intersections - north and south
+                        if (image[xPos - 1][yPos] == 0){
+                            paths.push(new PathPoint(xPos, yPos, DIRECTION.NORTH));
+                        }
+                        if (image[xPos + 1][yPos]==0){
+                            paths.push(new PathPoint(xPos, yPos, DIRECTION.SOUTH));
+                        }
+
+                        yPos++;
+                    }
+                    break;
+            }
+        }
+
+        System.out.printf("Solution starting at x=%d, y=%d", x, y);
+        System.out.println(paths.toString());
     }
+
+    public static void populateMaze(int[][] maze){
+        final String mazeFile = "./map.txt";
+        final int mazeHeight = maze.length;
+        final int mazeWidth = maze[0].length;
+        try {
+            //populate maze
+            Scanner in = new Scanner(new File(mazeFile));
+            int x = 0;
+            int y = 0;
+            while (in.hasNextInt()) {
+                int val = in.nextInt();
+                maze[x][y] = val;
+                y++;
+                //new line
+                if (y == 8) {
+                    y = 0;
+                    x++;
+                }
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("Error reading file");
+        }
+    }
+
+
+
 }
--- src/main/java/org/example/Question11.java
+++ src/main/java/org/example/Question11.java
@@ -1,13 +1,88 @@
 package org.example;
 
+import java.io.File;
+import java.util.*;
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Christopher Healy
+ *  Class Group: SD2A
  */
 public class Question11
 {
 
     public static void main(String[] args) {
+        Map<String, TreeSet<DistanceTo>> directConnections = new HashMap<>();
+        //populate map with distances from txt file, and set starting point as first city in text file
+        String startingPoint = populateMap(directConnections);
+        System.out.println("Starting Point: "+ startingPoint);
+
+        //start algorithm
+        String from = startingPoint;
+        Queue<DistanceTo> distancesQueue = new PriorityQueue<>();
+        distancesQueue.add(new DistanceTo(from, 0));
+        Map<String, DistanceTo> shortestKnownDistance = new HashMap<>();
+
+        while (!distancesQueue.isEmpty()){
+            //pull from top of queue then eval its shortest distance
+            DistanceTo minDistance = distancesQueue.poll();
+            String target = minDistance.getTarget();
+            int dist = minDistance.getDistance();
+            if (shortestKnownDistance.get(target)==null){
+                //add shortest dist of new cities
+                shortestKnownDistance.put(target, new DistanceTo(target, dist));
+
+                //check if target has direct connections
+                if (directConnections.get(target)!=null){
+                    //check the connections from each city, if their shortest dist not found, push them to q to be evaluated next
+                    for (DistanceTo dt: directConnections.get(target)){
+                        if (shortestKnownDistance.get(dt.getTarget())==null){
+                           distancesQueue.add(new DistanceTo(dt.getTarget(), dist + dt.getDistance()));
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("Results:");
+        for (Map.Entry entry: shortestKnownDistance.entrySet()){
+            DistanceTo distTo = (DistanceTo) entry.getValue();
+            int distance = distTo.getDistance();
+            System.out.println(entry.getKey() + ": " + distance + "mi");
+        }
+    }
+
+    public static String populateMap(Map<String, TreeSet<DistanceTo>> map){
+        try {
+            Scanner kb = new Scanner(new File("./citiesData.txt"));
+            String startingPoint = "";
+            boolean startSet = false;
+            while (kb.hasNextLine()){
+                String name = kb.next();
+                String dest = kb.next();
+                int distance = kb.nextInt();
+
+                //set start point to first line
+                if (!startSet){
+                    startingPoint = name;
+                    startSet = true;
+                }
+
+                //first occurance
+                if (map.get(name)==null){
+                    map.put(name, new TreeSet());
+                    map.get(name).add(new DistanceTo(dest, distance));
+                }
+                else {
+                    map.get(name).add(new DistanceTo(dest, distance));
+                }
+            }
+
+            return startingPoint;
+
+        } catch (Exception e){
+            System.out.println("Error while retrieving file");
+        }
+        return null;
 
     }
 }
